6c512701ebb43c3232fa9906402d8a9a50f719dc
secure73 <secure73@gmail.com>
2023-08-01 11:10:47 +0100

    convert image to base64

--- tobase.php
+++ tobase.php
@@ -1,6 +1,18 @@
 <?php
 //this convert image to base64 
 $image = "./img/sherek.jpeg";
+
+// Bild als Bytes lesen
+$imageBytes = file_get_contents($image);
+
+// Bild in Base64-Text umwandeln
+$base64Text = base64_encode($imageBytes);
+
 ?>
 
 <img height="300px" src=<?=$image?> />
+
+<hr>
+<h1>hier ist base64:</h1>
+
+<?=$base64Text?>
