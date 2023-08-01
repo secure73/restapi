8828a8602221c237d3205b4ad32b5c6b7c6970a4
secure73 <secure73@gmail.com>
2023-08-01 11:38:42 +0100

    echo json rest api

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $hello = "Hi it is php echo!!";
 $welcome = "<h1>welcome to php echo</h1>";
 $table = "<table><tr><td>sfsfsfdsddf</td></tr></table></td></tr></table>";
 
-echo $hello;
+echo json_encode($hello);
 ?>
 
 
