f03305323eb92ffcba3b477df9a69e1518efdcc5
secure73 <secure73@gmail.com>
2023-08-01 10:10:33 +0100

    variables php

--- variables.php
+++ variables.php
@@ -1,8 +1,9 @@
 <?php
+//define variable
 $hello = "Hi it is php echo!!";
 $welcome = "<h1>welcome to php echo</h1>";
 $table = "<table><tr><td>sfsfsfdsddf</td></tr></table></td></tr></table>";
 ?>
 
-
+/*-----------------use variable-----------------------------------------------*/
 <h2><?=$welcome?></h2>
